8127509d50972b808e2b1e07da6bb13389fa71eb
Kelna <kelnasebastian@gmail.com>
2026-06-10 10:56:48 +0530

    Added MongoDB Atlas connection

---
+++ MongoDB-Test/index.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+
+const uri = "const uri = "mongodb+srv://kelnasv09_db_user:ZtX2HQ68TFUb4098@cluster0.dtoyxkj.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0";";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("✅ Connected to MongoDB Atlas");
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.close();
+  }
+}
+
+run();
